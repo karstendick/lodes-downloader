fabed11639dbe80f56b4a37b96aab43aad40a82c
Joshua Karstendick <joshdick@gmail.com>
2018-09-03 16:29:41 -0400

    project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lodes-downloader
 Package to download Census LODES data
+
+## Setup
+
+```bash
+python3 -m venv venv
+source venv/bin/activate
+python setup.py develop
+```
+
+## Running the program
+
+```bash
+python lodes_downloader
+```
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='lodes_downloader',
+    version='0.0.1',
+    description='Package to download Census LODES data',
+    author='Joshua Karstendick',
+    url='https://github.com/karstendick/lodes-downloader',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'wget==3.2',
+        'beautifulsoup4==4.6.3',
+    ],
+)
